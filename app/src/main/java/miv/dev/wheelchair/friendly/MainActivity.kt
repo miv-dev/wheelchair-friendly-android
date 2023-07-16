01b3ec84e720095723c82ffe8262e27ef1f66365
miv-dev <i@mike39.ru>
2023-07-16 11:48:56 +0200

    Initial Commit

---
+++ app/src/main/java/miv/dev/wheelchair/friendly/MainActivity.kt
@@ -0,0 +1,30 @@
+package miv.dev.wheelchair.friendly
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import miv.dev.wheelchair.friendly.ui.theme.WheelchairFriendlyTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WheelchairFriendlyTheme {
+
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+
+}
